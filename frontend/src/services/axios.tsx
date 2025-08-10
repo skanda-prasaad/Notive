037150c41c8530e16b0fb432811fde4fbd502534
Skanda Prasad <skandaprasad595@gmail.com>
2025-08-10 16:10:42 +0530

    Update axios.tsx

--- frontend/src/services/axios.tsx
+++ frontend/src/services/axios.tsx
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:2000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:2000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
